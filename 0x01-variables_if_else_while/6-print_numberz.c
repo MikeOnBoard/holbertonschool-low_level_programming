9f0cd78790aff11c563d790f59ec0ba5263c1a75
MikeOnBoard <jeanran97@gmail.com>
2024-07-21 13:17:14 -0500

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* Anotaciones para poder entender el programa
+1. al tratarse de char en vez de int usando la funcion putchar se deben implementar las comillas simples('') para definir la variable numberz y asi mismo para darle el rango.
+2. siendo la declaracion completa dentro del bucle for, no habra necesidad de colocar las llaves de entrada y de salida {}.
+*/
+
+int main(void)
+{
+    char numberz;
+
+    for ( numberz = '0'; numberz <= '9'; numberz++)
+    putchar(numberz);
+    putchar('\n');
+    return (0);
+
+}
