aa03b36e9ac9c8dddc05e94557374ac7a13b0a0b
MikeOnBoard <jeanran97@gmail.com>
2024-07-23 11:52:21 -0500

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * Anotaciones para entender el programa realizado
+ */
+
+int _isalpha(int c)
+{
+    if ( c >= 65 && c <= 122 )
+    {
+    return(1);
+    }
+    return(0);
+}
