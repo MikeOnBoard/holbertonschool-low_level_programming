fdd8e15a0c6edfb13034f2bc5a4ff19df898efae
MikeOnBoard <jeanran97@gmail.com>
2024-07-23 07:43:35 -0500

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* main - Entry point
+* Return: Always 0
+* cuando se hace un programa que trae una libreria creada por nosotros y se incluye un archivo base donde se encuentra la funcion principal iterada a este archivo se debe incluir este archivo.
+* asi cuando se ejecute el comando -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c(archivo base que contiene la funcion a usar ) 0-putchar.c(archivo a compilar que trae las instrucciones a realizar) -o 0-putchar(archivo final compilado ejecutable)
+*/
+
+int main(void)
+
+{
+
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return (0);
+}
