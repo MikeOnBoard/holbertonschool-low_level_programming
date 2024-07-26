5057183dc4b05f8574b254c9438935608d87568c
MikeOnBoard <jeanran97@gmail.com>
2024-07-25 21:46:04 -0500

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/** Anotaciones para este programa
+ * realizaremos una multiplicacion
+ * al tratarse de dos enteros y que el final de la declaracion de la funcion prototipo(tipo entero).
+ * al final simplemente devolveremos el resultado de la multiplicacion.
+ * el resultado sera entregado por esta funcion pero los valores de la funcion seran entregados por la funcion main que llama a esta funcion dentro del programa. 
+ */
+
+
+int mul(int a, int b)
+{
+    int mul = a * b; 
+    return(mul);
+}
