07490eee24940eeebe552eafdcd2b13aee57f2fb
MikeOnBoard <jeanran97@gmail.com>
2024-07-29 21:59:29 -0500

    2-strcpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * 
+ * 
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+    char *start = dest;
+    int i;
+
+    for (i = 0; i < n; i++) {
+        if (i < n && src[i] != '\0') {
+            dest[i] = src[i];
+        } else {
+            dest[i] = '\0';
+        }
+    }
+
+    return start;
+}
